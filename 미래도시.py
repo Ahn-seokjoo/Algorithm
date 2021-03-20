a052f48589d67b448d0a3f3b4615a1543e612a8a
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-03-20 23:35:30 +0900

    미래도시
    플로이드 워셜 통한 1부터 k를 지나 x로 가는 최단 거리

--- 미래도시.py
+++ 미래도시.py
@@ -1,21 +1,21 @@
 INF = int(1e9)
 n, m = map(int,input().split())
+graph = [[INF]*(n+1) for _ in range(n + 1)]
 
-graph = [[INF]*(n+1) for _ in range (n+1)]
+#자신 > 자신 0으로
+for a in range(n+1):
+  for b in range(n+1):
+    if a == b:
+      graph[a][b] = 0
 
-for a in range(1,n+1):
-  for b in range(1,n+1):
-    if a==b:
-      graph[a][b] == 0
-# 각 간선에 대한 정보를 입력, 초기화
+#연결 도로 입력
 for _ in range(m):
-  #A와 B가 서로에게 가는 비용 1
   a,b = map(int,input().split())
   graph[a][b] = 1
   graph[b][a] = 1
 
 x, k = map(int,input().split())
-
+#플로이드 워셜 이용한 초기화
 for k in range(1,n+1):
   for a in range(1,n+1):
     for b in range(1,n+1):
@@ -23,7 +23,7 @@ for k in range(1,n+1):
 
 distance = graph[1][k] + graph[k][x]
 
-if distance >= INF:
+if distance >=INF:
   print("-1")
 else:
   print(distance)
