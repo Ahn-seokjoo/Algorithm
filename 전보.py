87012f583ed045d260196ae664ef633136a4528c
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-03-21 00:22:24 +0900

    전보
    전형적인 진보된 다익스트라로 우선순위 큐(heapq)를 통해 문제 해결, 끝에 도달 가능 노드 수 와 최단거리중 총 걸리는 시간(거리)을 추가해줌 . 
    입력이 많을 경우에 대비해 sys를 import 후 input = sys.stdin.readline을 해줌

--- 전보.py
+++ 전보.py
@@ -1,19 +1,20 @@
+# 도시 개수 n, 통로개수 m, 메시지를 보내고픈 도시 start
 import sys
 import heapq
 input = sys.stdin.readline
-INF = int(1e9)
 
 n,m,start = map(int,input().split())
 
-graph = [[] for i in range(n + 1)]
-distance = [INF] * (n+1)
+graph = [[] for i in range(n+1)]
+INF = int(1e9)
+distance = [INF]* (n+1)
 
 for _ in range(m):
   x,y,z = map(int,input().split())
   graph[x].append((y,z))
 
 def dijkstra(start):
-  q=[]
+  q = []
   heapq.heappush(q,(0,start))
   distance[start] = 0
   while q:
@@ -27,15 +28,18 @@ def dijkstra(start):
         heapq.heappush(q,(cost,i[0]))
 
 dijkstra(start)
-
+# 도달 가능한 노드 수
 count = 0
+# 도달 가능한 노드 중 가장 먼 노드와의 최단 거리
 max_distance = 0
+
 for d in distance:
-  #도달 가능하다면
-  if d != INF:
-    count +=1
-    max_distance = max(max_distance, d)
-#시작 노드 뺴야해서 count -1
+  if d!= INF:
+    count += 1
+    max_distance = max(max_distance,d)
+
+# 시작 노드 제외해야 해서 -1 해준다
 print(count-1,max_distance)
 
+    
 
