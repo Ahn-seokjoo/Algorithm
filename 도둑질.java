6bda03219aabea1ed6e1d6684637ebec6317b44f
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-01 22:01:33 +0900

    도둑질(DP) java ver
    https://programmers.co.kr/learn/courses/30/lessons/42897

---
+++ 도둑질.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] money) {
+        int[] dp = new int[money.length];
+       
+        int answer = 0;
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0],money[1]);
+        
+        for(int i=2;i<money.length-1;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+money[i]);
+        }
+        
+        answer = dp[money.length-2];
+        dp[0] = 0;
+        dp[1] = money[1];
+        
+        for(int i =2;i<money.length;i++){
+            dp[i] = Math.max(dp[i-1],dp[i-2]+money[i]);
+        }
+        answer = Math.max(answer,dp[money.length-1]);
+        return answer;
+    }
+}
