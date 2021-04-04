f3976240b0862e87d2e9640d64040b872ae1aeec
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-04 21:51:48 +0900

    숫자 카드 게임

--- 숫자 카드 게임.py
+++ 숫자 카드 게임.py
@@ -1,9 +1,12 @@
-n , m = map(int,input().split())
+n, m = map(int,input().split())
 
-result =0
+result = 0
+
 for i in range(n):
-  data = list(map(int,input().split())
+  data = list(map(int,input().split()))
   min_value = min(data)
+
   result = max(result,min_value)
 
 print(result)
+
