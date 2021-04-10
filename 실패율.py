eabe39d3aa9123b285f477632d47e0f4c42fbe9b
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-10 23:06:08 +0900

    실패율(정렬,구현)
    https://programmers.co.kr/learn/courses/30/lessons/42889#

--- 실패율.py
+++ 실패율.py
@@ -15,3 +15,28 @@ def solution(N, stages):
     answer = sorted(answer,key=lambda t: t[1], reverse=True)
     answer = [i[0] for i in answer]
     return answer
+
+def solution(N, stages):
+    answer=[]
+    failure = []
+    new_answer = []
+    count = [0] *(N + 2)
+    length = len(stages)
+    
+    for i in range(len(stages)):
+        count[stages[i]] += 1
+    
+    for i in range(1,len(count)):
+        if count[i] != 0:
+            failure.append(float(count[i]/length))
+            length -= count[i]
+        else:
+            failure.append(int(0))
+    
+    for i in range(N):
+        answer.append((i+1,failure[i]))
+    answer.sort(key = lambda x: -x[1])
+    for x in answer:
+        new_answer.append(x[0])
+        
+    return new_answer
