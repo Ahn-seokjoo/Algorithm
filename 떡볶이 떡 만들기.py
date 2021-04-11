cad265470713293b6af4f68342a65f477f2ef840
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-12 00:08:41 +0900

    떡볶이 떡 만들기(파라메트릭 서치, 이진 탐색)

--- 떡볶이 떡 만들기.py
+++ 떡볶이 떡 만들기.py
@@ -1,49 +1,24 @@
-//이진 탐색, 파라메트릭 서치!
-n, m = list(map(int,input().split(' ')))
-
+n, m = map(int,input().split())
 array = list(map(int,input().split()))
 
-start = 0 
+#시작점 끝점 설정
+start = 0
 end = max(array)
 
-result=0
-while(start<=end):
-  total = 0
-  mid = (start + end) //2
-
-  for x in array:
-    if x>mid:
-      total += x - mid
-    
-  if total < m:
-    end = mid-1
-  else:
-    result = mid
-    start = mid + 1
-
-print(result)
-
-#### 2회 풀이
-
-n, m = list(map(int,input().split(' ')))
-array = list(map(int,input().split()))
-
-start = 0 
-end = max(array)
-
+#이진 탐색 수행
 result = 0
 while(start<=end):
   total = 0
   mid = (start + end) // 2
   for x in array:
+    # 잘랐을 때의 떡의 양 계산
     if x > mid:
-      #잘랐을 때의 양 = x - mid 을 total에 더해주어 총 자른 양을 구함
       total += x - mid
+  # 자른 떡이 부족한경우 더 많이 자르기
   if total < m:
-    #떡의 양이 적으면 왼쪽을 늘려 떡의 길이를 늘려준다
-    end = mid -1
+    end = mid - 1
+  # 충분한 경우 덜 자르기
   else:
-    #충분한 경우 오른쪽에서 덜 자르기
-    result = mid #최대한 덜 잘랐을 때가 정답, 
+    result = mid #최대한 덜자른 것이 정답
     start = mid + 1
 print(result)
