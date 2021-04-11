e1bba4b11183e5ab037000eec5a74b59c577edde
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-12 02:17:57 +0900

    고정점 찾기(이진 탐색)

--- 고정점 찾기.py
+++ 고정점 찾기.py
@@ -1,14 +1,12 @@
 def binary_search(array,start,end):
-  if start> end:
+  if start > end :
     return None
   mid = (start + end) // 2
-  #고정점을 찾은 경우 인덱스 반환
+ # target 없이 mid 와 array[mid] 값이 같다면 출력
   if array[mid] == mid:
     return mid
-  #중간점이 위치의 값보다 중간점이 작은 경우 왼쪽을 확인
   elif array[mid] > mid:
     return binary_search(array,start,mid-1)
-  #중간점이 가리키는 위치의 값보다 큰 경우 오른쪽을 확인
   else:
     return binary_search(array,mid+1,end)
 
@@ -16,7 +14,6 @@ n = int(input())
 array = list(map(int,input().split()))
 
 index = binary_search(array,0,n-1)
-
 if index == None:
   print(-1)
 else:
