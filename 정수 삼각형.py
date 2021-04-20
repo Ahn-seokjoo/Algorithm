ded003868b838f8c4fced34c6a72a81c02272cca
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-20 11:40:40 +0900

    정수 삼각형(DP)

--- 정수 삼각형.py
+++ 정수 삼각형.py
@@ -2,15 +2,13 @@ n = int(input())
 dp = []
 for _ in range(n):
   dp.append(list(map(int,input().split())))
-for i in range(1,n):
-  for j in range(i+1):
-    if j == 0:
-      up_left = 0
+
+for i in range(1,len(dp)):
+  for j in range(len(dp[i])):
+    if(j == 0):
+      dp[i][j] += dp[i-1][j]
+    elif(j == i):
+      dp[i][j] += dp[i-1][j-1]
     else:
-      up_left = dp[i-1][j-1]
-    if j == i:
-      up = 0
-    else:
-      up = dp[i-1][j]
-    dp[i][j] = dp[i][j] + max(up_left,up)
+      dp[i][j] += max(dp[i-1][j-1],dp[i-1][j])
 print(max(dp[n-1]))
