be76108c3ccfba8aea5e710ac35bc2d97f0886af
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-23 20:38:56 +0900

    플로이드 워셜(DP, 최단경로) 간선의 수가 적을때 사용 가능

--- 플로이드 워셜.py
+++ 플로이드 워셜.py
@@ -1,31 +1,28 @@
 INF = int(1e9)
 
-#노드 및 간선의 수
 n = int(input())
 m = int(input())
 
-# 2차원 그래프를 만든 뒤 모두 inf로 초기화
-graph =[[INF]*(n+1) for _ in range(n+1)]
-# 자신에게 가는 비용은 모두 0
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
 for a in range(1,n+1):
   for b in range(1,n+1):
-    if a==b:
+    if a == b :
       graph[a][b] = 0
 
 for _ in range(m):
-  #간선 정보 받기
   a,b,c = map(int,input().split())
   graph[a][b] = c
 
-for k in range(1, n+1):
-  for a in range(1, n+1):
-    for b in range(1, n+1):
+for k in range(1,n+1):
+  for a in range(1,n+1):
+    for b in range(1,n+1):
       graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
 
 for a in range(1,n+1):
   for b in range(1,n+1):
     if graph[a][b] == INF:
-      print("INF", end=" ")
+      print('INF')
     else:
-      print(graph[a][b],end=" ")
+      print(graph[a][b],end=' ')
   print()
