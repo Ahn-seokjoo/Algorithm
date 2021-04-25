c5da38695322acc41a9ab37430c073acf43663bd
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-25 19:32:12 +0900

    전보(최단경로, 다익스트라)

--- 전보.py
+++ 전보.py
@@ -1,13 +1,12 @@
-# 도시 개수 n, 통로개수 m, 메시지를 보내고픈 도시 start
-import sys
 import heapq
+import sys
+
 input = sys.stdin.readline
-
-n,m,start = map(int,input().split())
-
-graph = [[] for i in range(n+1)]
 INF = int(1e9)
-distance = [INF]* (n+1)
+
+n, m, start = map(int,input().split())
+graph = [[] for i in range(n+1)]
+distance = [INF] * (n+1)
 
 for _ in range(m):
   x,y,z = map(int,input().split())
@@ -26,20 +25,16 @@ def dijkstra(start):
       if cost < distance[i[0]]:
         distance[i[0]] = cost
         heapq.heappush(q,(cost,i[0]))
-
 dijkstra(start)
-# 도달 가능한 노드 수
-count = 0
-# 도달 가능한 노드 중 가장 먼 노드와의 최단 거리
-max_distance = 0
 
+count = 0
+max_distance = 0
 for d in distance:
-  if d!= INF:
+  if d != INF:
     count += 1
     max_distance = max(max_distance,d)
 
-# 시작 노드 제외해야 해서 -1 해준다
-print(count-1,max_distance)
+print(count -1, max_distance)
+  
 
-    
 
