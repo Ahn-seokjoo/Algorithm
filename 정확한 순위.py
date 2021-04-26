52868e8b1377930973cf2efe916652f2058d06d8
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-26 19:46:39 +0900

    정확한 순위(플로이드워셜 + 끝에 조금 수정)

--- 정확한 순위.py
+++ 정확한 순위.py
@@ -1,26 +1,25 @@
-import sys
-input = sys.stdin.readline
-n,m = map(int,input().split())
-
 INF = int(1e9)
-graph = [[INF]*(n+1) for _ in range(n+1)]
 
-for a in range(1,n+1):
-  for b in range(1,n+1):
-    if a==b:
-      graph[a][b] = 0
+n, m = map(int,input().split())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
   a,b = map(int,input().split())
   graph[a][b] = 1
 
+for a in range(1,n+1):
+  for b in range(1,n+1):
+    if a == b:
+      graph[a][b] = 0
+
 for k in range(1,n+1):
   for a in range(1,n+1):
     for b in range(1,n+1):
-      graph[a][b] = min(graph[a][b],graph[a][k] + graph[k][b])
-
+      graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
 
 result = 0
+
 for a in range(1,n+1):
   count = 0
   for b in range(1,n+1):
@@ -28,6 +27,6 @@ for a in range(1,n+1):
       count += 1
   if count == n:
     result += 1
-  
 
 print(result)
+    
