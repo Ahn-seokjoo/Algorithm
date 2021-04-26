930fc89df2d520ce7e619b20893d8a002dccc5ab
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-26 21:23:13 +0900

    경로압축 적용, 서로소 집합 알고리즘

--- 발전된 서로소 집합 알고리즘.py
+++ 발전된 서로소 집합 알고리즘.py
@@ -1,10 +1,8 @@
 def find_parent(parent,x):
-  # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
   if parent[x] != x:
-    parent[x] = find_parent(parent,parent[x])
+    parent[x] = find_parent(parent, parent[x])
   return parent[x]
 
-#두 원소가 속한 집합 합치기
 def union_parent(parent,a,b):
   a = find_parent(parent,a)
   b = find_parent(parent,b)
@@ -13,25 +11,22 @@ def union_parent(parent,a,b):
   else:
     parent[a] = b
 
-# 노드의 개수와 간선의 개수 입력
-v, e = map(int, input().split())
-parent = [0] * (v+1) # 부모 테이블 초기화
+v, e = map(int,input().split())
+parent = [0] * (v+ 1)
 
-#테이블에서 자기 자신의 부모를 자기로 초기화
 for i in range(1,v+1):
   parent[i] = i
 
-# union 수행
 for i in range(e):
-  a, b =map(int,input().split())
-  union_parent(parent, a, b)
+  a,b = map(int,input().split())
+  union_parent(parent,a,b)
 
-print('각 원소가 속한 집합: ',end='')
+print('각 원소가 속한 집합: ',end=' ')
 for i in range(1,v+1):
   print(find_parent(parent,i),end=' ')
 
 print()
-# 부모 테이블 내용 출력
-print('부모 테이블: ',end='')
+
+print('부모 테이블',end=' ')
 for i in range(1,v+1):
   print(parent[i],end=' ')
