3724848cefef0c980bebdfb1c56321e2a66b5b75
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-04-28 23:13:45 +0900

    문자열 압축(구현,문자열 처리)

--- 문자열 압축.py
+++ 문자열 압축.py
@@ -1,20 +1,18 @@
 def solution(s):
     answer = len(s)
-    # 1개 단위부터 늘려가며 확인, 중간까지
-    for step in range(1,len(s) // 2 + 1):
+    for step in range(1,len(s)//2 +1):
         compressed = ""
-        prev = s[0:step]
+        prev = s[:step]
         count = 1
         for j in range(step,len(s),step):
             if prev == s[j:j+step]:
                 count += 1
             else:
                 compressed += str(count) + prev if count >= 2 else prev
-                prev = s[j:j+step]
                 count = 1
-        #남은 문자열 처리
-        compressed += str(count) + prev if count >= 2 else prev
-        #만들어지는 것들 중 가장 짧은 것이 정답
+                prev = s[j:j+step]
+        
+        compressed += str(count) + prev if count >= 2 else prev # 나머지 처리, 3c는 만들었으나 붙여주는 코드가 없음. so 여기서 붙어줌
+        
         answer = min(answer,len(compressed))
-    
     return answer
