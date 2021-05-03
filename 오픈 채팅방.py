2c16736db50399bdf87fba4ccaeb13fac19a1ac3
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-05-04 01:20:31 +0900

    오픈 채팅방(딕셔너리, 문자열 처리)
    https://programmers.co.kr/learn/courses/30/lessons/42888

---
+++ 오픈 채팅방.py
@@ -0,0 +1,12 @@
+def solution(records):
+    records = list(map(lambda r: r.split(),records))
+    users = {}
+    for record in records:
+        if len(record) > 2:
+            users[record[1]] = record[2]
+    msg = {'Enter' : '님이 들어왔습니다.', 'Leave' : '님이 나갔습니다.'}
+    msgs = []
+    for record in records:
+        if record[0] != 'Change':
+            msgs.append(users[record[1]] + msg[record[0]])
+    return msgs
