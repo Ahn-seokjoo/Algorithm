a1a3958dafc5f2e5bd8fa2637a6d005da227e0a9
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-05-07 23:45:24 +0900

    경주로 건설(bfs,큐)
    https://programmers.co.kr/learn/courses/30/lessons/67259

--- 경주로 건설.py
+++ 경주로 건설.py
@@ -4,21 +4,19 @@ INF = int(1e9)
 def solution(board):
     length = len(board)
     def bfs(start):
-        table = [[INF] * length for _ in range(length)]
+        table = [[INF]*length for _ in range(length)]
+        dirs = [(-1,0),(0,-1),(1,0),(0,1)] #좌,위,우,아래
         
-        dirs= [(-1,0),(0,-1),(1,0),(0,1)]
-        queue = deque([start])
-        
+        q = deque([start])
         table[0][0] = 0
-        while queue:
-            y,x,cost,head = queue.popleft()
-            for idx,(dy,dx) in enumerate(dirs):
-                ny ,nx = y+dy ,x+dx
+        while q:
+            x,y,cost,dist = q.popleft()
+            for idx,(dx,dy) in enumerate(dirs): # enum 이용해 인덱스도 같이 뽑아내서 방향까지 계산
+                nx,ny = x + dx ,y + dy
+                n_cost = cost + 600 if idx != dist else cost + 100
                 
-                n_cost = cost + 600 if idx != head else cost + 100
-                
-                if 0 <= ny < length and 0 <= nx < length and board[ny][nx] == 0 and table[ny][nx] > n_cost:
-                    table[ny][nx] = n_cost
-                    queue.append((ny,nx,n_cost,idx))
+                if 0 <= nx < length and 0 <= ny < length and board[nx][ny] == 0 and table[nx][ny] > n_cost:
+                    table[nx][ny] = n_cost #테이블에는 값을 계속해서 저장
+                    q.append((nx,ny,n_cost,idx))
         return table[-1][-1]
     return min(bfs((0,0,0,2)),bfs((0,0,0,3)))
