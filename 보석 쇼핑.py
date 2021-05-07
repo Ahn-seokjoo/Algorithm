12d375025a3cab27d0a36fa5887e427ff64bd64a
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-05-07 23:39:14 +0900

    보석 쇼핑(투포인터,딕셔너리)
    https://programmers.co.kr/learn/courses/30/lessons/67258

--- 보석 쇼핑.py
+++ 보석 쇼핑.py
@@ -1,28 +1,28 @@
 def solution(gems):
     answer = []
-    shortest = len(gems) + 1
-    len_check = len(set(gems))
-    dict = {}
+    dict = {} # 현재 구간에 포함된 보석들
+    shortest = len(gems) + 1 # 현재 최단 구간 길이
     
-    start = 0
-    end = 0
+    len_check = len(set(gems))
+    start,end = 0,0 # 시작, 끝점
     
     while end < len(gems):
-        if gems[end] not in dict:
+        if gems[end] not in dict: # 끝점의 보석이 없다면 추가
             dict[gems[end]] = 1
         else:
             dict[gems[end]] += 1
-        end += 1
+        end += 1 #끝점 증가
         
-        if len_check == len(dict): # 다 들어갔을 때
-            while start < end:
-                if dict[gems[start]] > 1:
+        if len(dict) == len_check: # 다 들어가 있다면
+            while start < end: #start 가 end 보다 같을떄까지 증가
+                if dict[gems[start]] > 1: # 보석 하나씩 빼보기
                     dict[gems[start]] -= 1
                     start += 1
-                elif shortest > end - start:
+                elif shortest > end - start: # 기존의 구간 최단거리보다 짧다면
                     shortest = end - start
-                    answer = [start+1,end]
+                    answer = [start + 1,end] #답과 최단거리 갱신
                     break
                 else:
-                    break    
+                    break
+
     return answer
