0bfbe696a2c00c17c94bd7b4017e0544dd3ef594
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-06-05 16:09:31 +0900

    DFS 기초 ,스택

--- DFS.py
+++ DFS.py
@@ -1,11 +1,18 @@
-def dfs(graph,v,visited):
+def dfs(graph,v,visted):
   visited[v] = True
   print(v,end=' ')
-
   for i in graph[v]:
     if not visited[i]:
-      dfs(graph,i,visited)
+      dfs(graph,i,visited) # 재귀적으로 호출
 
-visited = [False] * 9
-graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
-dfs(graph,1,visited)
+graph = [[],
+[2,3,8],
+[1,7],
+[1,4,5],
+[3,5],
+[3,4],
+[7],
+[2,6,8],
+[1,7]]
+visited = [False] * 9 # 처음은 False
+dfs(graph,1,visited) # 1 부터 시작
