f5cb7b2c5f05785d7b4383cc4289669519308e74
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-06-05 16:17:31 +0900

    BFS 큐

--- BFS.py
+++ BFS.py
@@ -3,15 +3,23 @@ from collections import deque
 def bfs(graph,start,visited):
   queue = deque([start])
   visited[start] = True
-
-  while queue:
+  while queue: #큐가 빌 떄까지
     v = queue.popleft()
     print(v,end=' ')
     for i in graph[v]:
-      if not visited[i]:
-        queue.append(i)
+      if not visited[i]: #방문 안했다면
+        queue.append(i) #큐에 다시 넣기
         visited[i] = True
 
+graph= [[],
+[2,3,8],
+[1,7],
+[1,4,5],
+[3,5],
+[3,4],
+[7],
+[2,6,8],
+[1,7]]
+
 visited = [False] * 9
-graph = [[],[2,3,8],[1,7],[1,4,5],[3,5],[3,4],[7],[2,6,8],[1,7]]
 bfs(graph,1,visited)
