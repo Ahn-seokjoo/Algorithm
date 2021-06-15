3c13dbcf35dd7dcd0d42b30567f91d086fbba4ef
Ahn-seokjoo <67602108+Ahn-seokjoo@users.noreply.github.com>
2021-06-15 22:31:06 +0900

    다트게임(정규표현식, 딕셔너리)

--- 다트 게임.py
+++ 다트 게임.py
@@ -18,3 +18,19 @@ def solution(dartResult):
         result.append(dart)
         
     return eval(''.join(result)[:-1])
+
+import re
+
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    
+    dart = p.findall(dartResult)            
+    for i in range(len(dart)):                # 정규표현식으로 갈무리된 문자열들
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+    answer = sum(dart)
+    return answer
